f50d3d098b224c26b372daaf8b2fc3b8d2ce575e
Avinash Dayal <avinashdayal1608@gmail.com>
2025-11-25 14:36:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function minCostRopes(ropes) {
+    if (ropes.length <= 1) {
+        return 0; // No cost if 0 or 1 rope
+    }
 
-module.exports=mincost;
+    const minHeap = new MinHeap();
+    for (const rope of ropes) {
+        minHeap.insert(rope);
+    }
+
+    let totalCost = 0;
+
+    while (minHeap.size() > 1) {
+        const firstMin = minHeap.extractMin();
+        const secondMin = minHeap.extractMin();
+
+        const currentCost = firstMin + secondMin;
+        totalCost += currentCost;
+
+        minHeap.insert(currentCost); // Add the new combined rope back to the heap
+    }
+
+    return totalCost;
+}
